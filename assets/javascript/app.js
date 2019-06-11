8dc41f679759552178dc6fbcad0158edd5549722
birna <briandam26@yahoo.com>
2019-06-11 12:58:57 -0700

    created loop to sift through and narrow down the pool of locations

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-
 const apiKeys = [
   skyscanner = "",
   yelp = "ETyIXGHKE8nskR_WJUaEvwJeXNjFJ5Cq_a_HdZNZmsTkzTut_-Y68XQPpCej1uyiIcmuW2PhP2j2rlSZMKmeecYZK8lOYImJNV9s00Su6K_Peuojo9vcupVUc5n-XHYx",
@@ -11,61 +10,6 @@ const apiKeys = [
 
 const pageDisplay = [".openingPage", ".secondPage", ".eventPage", ".foodPage", ".scheduler"];
 let pageDisplayBool = [true, false, false, false, false];
-
-const displayer = () => {
-  $(".openingPage").hide();
-  $(".secondPage").hide();
-  $(".eventPage").hide();
-  $(".foodPage").hide();
-  $(".scheduler").hide();
-  
-  if (pageDisplayBool[0]) {
-    $(".openingPage").show();
-  } if (pageDisplayBool[1]) {
-    $(".secondPage").show();
-  } if (pageDisplayBool[2]) {
-    $(".eventPage").show();
-  } if (pageDisplayBool[3]) {
-    $(".foodPage").show();
-  } if (pageDisplayBool[4]) {
-    $(".scheduler").show();
-  };
-};
-
-displayer();
-
-$(document).on("click", ".buttonStart", function(){
-  pageDisplayBool[0] = false;
-  pageDisplayBool[1] = true;
-  console.log(pageDisplayBool);
-  displayer();
-});
-
-$(document).on("click", ".eventsB", function() {
-  pageDisplayBool[1] = false;
-  pageDisplayBool[2] = true;
-  displayer();
-});
-
-$(document).on("click", ".foodPlacesB", function() {
-  pageDisplayBool[1] = false;
-  pageDisplayBool[3] = true;
-  displayer();
-});
-
-$(document).on("click", ".backButton", function() {
-  pageDisplayBool[2] = false;
-  pageDisplayBool[3] = false;
-  pageDisplayBool[4] = false;
-  pageDisplayBool[1] = true;
-  displayer();
-});
-
-$(document).on("click", ".restartButton", function() {
-  pageDisplayBool[1] = false;
-  pageDisplayBool[0] = true;
-  displayer();
-});
 
 //SLIDERS input functions to display a string at each range
 $('#safetyLvl').on("input", function() {
@@ -103,247 +47,354 @@ const genDestinations = [
   {
     name: "kyoto",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 1,
+    money: 2,
+    social: 2 
   }, 
   {
     name: "istanbul",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 2,
+    money: 2,
+    social: 2 
   },
   {
     name: "new york city",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 2,
+    money: 3,
+    social: 2 
   }, 
   {
     name: "bali",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 2,
+    money: 2,
+    social: 3 
   }, 
   {
     name: "florence",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 1,
+    money: 2,
+    social: 3 
   },
   {
     name: "amsterdam",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 2,
+    money: 3,
+    social: 3 
   },
   {
     name: "venice",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 1,
+    money: 3,
+    social: 1 
   },
   {
     name: "melbourne",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 3,
+    money: 3,
+    social: 3 
   },
   {
     name: "porto",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 2,
+    money: 1,
+    social: 3 
   },
   {
     name: "honolulu",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 3,
+    money: 2,
+    social: 3
   },
   {
     name: "salzburg",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 1,
+    money: 3,
+    social: 2 
   },
   {
     name: "paris",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 2,
+    money: 3,
+    social: 1
   },
   {
     name: "seychalles",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 3,
+    money: 2,
+    social: 2 
   }, 
   {
     name: "puerto vallarta",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 2,
+    money: 1,
+    social: 2 
   },
   {
     name: "las vegas",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 2,
+    money: 3,
+    social: 3 
   },
   {
     name: "taipei",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 1,
+    money: 1,
+    social: 3 
   },
   {
     name: "portland",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 1,
+    money: 1,
+    social: 2 
   },
   {
     name: "los angeles",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 2,
+    money: 2,
+    social: 3 
   },
   {
     name: "madrid",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 1,
+    money: 2,
+    social: 3 
   },
   {
     name: "lisbon",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 1,
+    money: 1,
+    social: 3 
   },
   {
     name: "sao paulo",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 3,
+    money: 3,
+    social: 2 
   }, 
   {
     name: "singapore",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 1,
+    money: 3,
+    social: 2 
   },
   {
     name: "vienna",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 1,
+    money: 2,
+    social: 1 
   },
   {
     name: "rio de janeiro",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 3,
+    money: 1,
+    social: 3 
   },
   {
     name: "osaka",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 1,
+    money: 2,
+    social: 3 
   },
   {
     name: "vancouver",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 2,
+    money: 2,
+    social: 1 
   }, 
   {
     name: "queenstown",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 2,
+    money: 2,
+    social: 1
   }, 
   {
     name: "el chalten",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 3,
+    money: 1,
+    social: 1 
   },
   {
     name: "coron",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 3,
+    money: 3,
+    social: 1 
   },
   {
     name: "moab",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 3,
+    money: 1,
+    social: 3 
   },
   {
     name: "puerto escondido",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 3,
+    money: 2,
+    social: 1 
   },
   {
     name: "nara",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 1,
+    money: 1,
+    social: 1 
   },
   {
     name: "victoria falls",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 3,
+    money: 1,
+    social: 2 
   },
   {
     name: "corsica",
     place_id: "",
-    safety: 0,
-    money: 0,
-    social: 0 
+    safety: 2,
+    money: 1,
+    social: 1 
+  },
+  {
+    name: "tokyo",
+    place_id: "",
+    safety: 1,
+    money: 3,
+    social: 3
   }
 ];//end ARRAY OF OBJECTS section
 
-//FUNCTION to take the 3 user inputs and randomly generate a single destination based on the criteria given
-//blah blah blah
-//end generateDestination FUNCTION section
+const displayer = () => {
+  $(".openingPage").hide();
+  $(".secondPage").hide();
+  $(".eventPage").hide();
+  $(".foodPage").hide();
+  $(".scheduler").hide();
+  
+  if (pageDisplayBool[0]) {
+    $(".openingPage").show();
+  } if (pageDisplayBool[1]) {
+    $(".secondPage").show();
+  } if (pageDisplayBool[2]) {
+    $(".eventPage").show();
+  } if (pageDisplayBool[3]) {
+    $(".foodPage").show();
+  } if (pageDisplayBool[4]) {
+    $(".scheduler").show();
+  };
+};
 
+displayer();
+
+//start GENERATING DESTINATION section
+$(document).on("click", ".buttonStart", function(){
+  pageDisplayBool[0] = false;
+  pageDisplayBool[1] = true;
+  console.log(pageDisplayBool);
+  displayer();
+
+  //stores the user's inputs from sliders
+  const userInput = {
+    safety: $("#safetyLvl").val(),
+    money: $("#moneyLvl").val(),
+    social: $("#socialLvl").val() 
+  };
+  //console.log(userInput); test worked
+
+  //make a new array of objects to hold the arrays that match  
+  let usersPool = [];
+  
+  //have a for loop sift through the array of objects
+  //have conditional statements to find destinations that match the user's inputs
+  for (let i=0; i<genDestinations.length; i++) {
+    if ($("#safetyLvl").val() === genDestinations[i].safety){
+      if ($("#moneyLvl").val() == genDestinations[i].money){
+        if ($("#socialLvl").val() == genDestinations[i].social){
+          usersPool.push(genDestinations[i].name);
+          //console.log("uno")
+        };
+      };
+    };
+    if ($("#safetyLvl").val() === genDestinations[i].safety){
+      if ($("#moneyLvl").val() == genDestinations[i].money){
+          usersPool.push(genDestinations[i].name);
+          //console.log("dos")
+      };
+    };
+    if ($("#safetyLvl").val() === genDestinations[i].safety){
+      if ($("#socialLvl").val() == genDestinations[i].social){
+        usersPool.push(genDestinations[i].name);
+        //console.log("tres")
+      };
+    };
+    if ($("#moneyLvl").val() == genDestinations[i].money){
+      if ($("#socialLvl").val() == genDestinations[i].social){
+        usersPool.push(genDestinations[i].name);
+        //console.log("quatro")
+      };
+    };
+  };
+  //console.log(usersPool);
+});
+
+//randomly pick from userPool array and display the new location
+
+//end GENERATING DESTINATION section
+
+
+$(document).on("click", ".eventsB", function() {
+  pageDisplayBool[1] = false;
+  pageDisplayBool[2] = true;
+  displayer();
+});
+
+$(document).on("click", ".foodPlacesB", function() {
+  pageDisplayBool[1] = false;
+  pageDisplayBool[3] = true;
+  displayer();
+});
+
+$(document).on("click", ".backButton", function() {
+  pageDisplayBool[2] = false;
+  pageDisplayBool[3] = false;
+  pageDisplayBool[4] = false;
+  pageDisplayBool[1] = true;
+  displayer();
+});
+
+$(document).on("click", ".restartButton", function() {
+  pageDisplayBool[1] = false;
+  pageDisplayBool[0] = true;
+  displayer();
+});
+  
 //--------------------------------------- start yelp food API section -------------------------------------------
 
 // Next to implement: button for "see more food" that switches out the 5 restaurants for the next 5 on the list, up to 25
